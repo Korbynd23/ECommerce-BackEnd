d4e3818a5b70b45a8848c2e18b3db338f26246c4
kdicari24@gmail.com <kdicari24@gmail.com>
2022-09-08 09:48:29 -0400

    saving changes for push

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,25 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: 'localhost',
     dialect: 'mysql',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
+    port: 3306,
+  }
+);
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     dialectOptions: {
+//       decimalNumbers: true,
+//     },
+//   });
 
 module.exports = sequelize;
